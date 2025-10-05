eef256653a3cdd4c88695df0868261bef7c8c42f
Acus <acuscodinghcm@gmail.com>
2025-10-06 01:56:13 +0700

    Phuc | Update views

--- internal/views/components/feature_templ.go
+++ internal/views/components/feature_templ.go
@@ -5,12 +5,10 @@ package components
 
 //lint:file-ignore SA4006 This context is only used if a nested component is present.
 
-import (
-	"dev-go-apis/internal/models"
+import "github.com/a-h/templ"
+import templruntime "github.com/a-h/templ/runtime"
 
-	"github.com/a-h/templ"
-	templruntime "github.com/a-h/templ/runtime"
-)
+import "dev-go-apis/internal/models"
 
 func Feature(status models.FeatureStatus, label string) templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
